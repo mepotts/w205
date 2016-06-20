6a1a237f42a07328dee6188fa2b164943d04b382
mepotts <mepotts@ischool.berkeley.edu>
2016-06-20 01:22:49 -0400

    Update load_data_lake.sh

--- exercise_1/loading_and_modelling/load_data_lake.sh
+++ exercise_1/loading_and_modelling/load_data_lake.sh
@@ -3,6 +3,7 @@ hdfs dfs -mkdir /user/w205/hospital_compare
 
 #download hopital files
 wget -O Hospital_Revised_Flatfiles.zip https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
+unzip Hospital_Revised_Flatfiles.zip
 
 tail -n +2 Hospital\ General\ Information.csv > hospitals.csv
 tail -n +2 Timely\ and\ Effective\ Care\ -\ Hospital.csv > effective_care.csv
