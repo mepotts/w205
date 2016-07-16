392ca1363d5ef2e7447e0c4ec3023c3747afcd1e
mepotts <mepotts@ischool.berkeley.edu>
2016-07-16 12:56:15 -0400

    Add files via upload

---
+++ lab6/wordcount.clj
@@ -0,0 +1,31 @@
+(ns wordcount
+(:use     [streamparse.specs])
+(:gen-class))
+
+(defn wordcount [options]
+ [
+  ;; senteces spout configuration
+  {"sentences-spout" (python-spout-spec
+        options
+        "spouts.sentences.Sentences"
+        ["sentence"]
+        )
+  }
+  ;; parse bolt configuration
+  {"parse-bolt" (python-bolt-spec
+        options
+        {"sentences-spout" :shuffle}
+        "bolts.parse.ParseTweet"
+        ["valid_words"]
+        :p 2
+        )
+  }
+  ;; tweetcounter bolt configuration
+  {"tweetcounter-bolt" (python-bolt-spec
+        options
+        {"parse-bolt" :shuffle}
+        "bolts.tweetcounter.TweetCounter"
+        ["word" "count"]
+        )
+  }
+]
