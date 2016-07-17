f319cf82e81371fdf4ad79df397e6739d8599efc
mepotts <mepotts@ischool.berkeley.edu>
2016-07-17 13:14:09 -0400

    Update sentences.py

--- lab6/sentences.py
+++ lab6/sentences.py
@@ -1,4 +1,5 @@
-from __future__ import absolute_import, print_function, unicode_literals import itertools
+from __future__ import absolute_import, print_function, unicode_literals 
+import itertools
 from streamparse.spout import Spout
 class Sentences(Spout):
   def initialize(self, stormconf, context):
