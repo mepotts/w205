94858ef847b85418dc8f202015c38f69ea26d21c
mepotts <mepotts@ischool.berkeley.edu>
2016-07-17 13:11:17 -0400

    Update parse.py

--- lab6/parse.py
+++ lab6/parse.py
@@ -1,4 +1,5 @@
-from __future__ import absolute_import, print_function, unicode_literals import re
+from __future__ import absolute_import, print_function, unicode_literals
+import re
 from streamparse.bolt import Bolt
 def ascii_string(s):
   return all(ord(c) < 128 for c in s)
